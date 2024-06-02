8c0a5e2305e33dec9daff3e8452f42f43b00bfe9
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-06-02 10:14:37 +0330

    a Bug In Register page

--- scripts/register.js
+++ scripts/register.js
@@ -2,43 +2,29 @@
 let formUsernameInput = document.querySelector(".form__username-input");
 let formPasswordInput = document.querySelector(".form__password-input");
 let formEmailInput = document.querySelector(".form__email-input");
+
+let formFirstnameInput = document.querySelector('.form__firstname-input')
+let formLastnameInput = document.querySelector('.form__lastname-input')
 let formBtn = document.querySelector(".form-btn");
 
-let usernameValid,
-  passwordValid,
-  emailValid = null;
 formBtn.addEventListener("click", (event) => {
   event.preventDefault();
   if (
-    formUsernameInput.value.length < 8 ||
-    formPasswordInput.value.length < 8
+    formUsernameInput.value.length < 4 ||
+    !validateEmail(formEmailInput.value)
   ) {
     iziToast.show({
       title: "Error",
-      message: "Please Enter Valid Username And Password",
-      position: "topRight",
+      message:
+        "Please fill in the fields correctly (enter username and password more than 4 characters and email in the correct form)",
+      position: "topCenter",
       titleSize: "20px",
       messageSize: "16px",
       titleColor: "#ffff",
       backgroundColor: "#ff204e",
       messageColor: "#ffff",
     });
-    usernameValid = false;
-    passwordValid = false;
-  } else if (!validateEmail(formEmailInput.value)) {
-    iziToast.show({
-      title: "Error",
-      message: "Please Enter Valid Email :(",
-      position: "topRight",
-      titleSize: "20px",
-      messageSize: "16px",
-      titleColor: "#ffff",
-      backgroundColor: "#ff204e",
-      messageColor: "#ffff",
-    });
-    emailValid = false;
   } else {
-    console.log("fetch");
     iziToast.show({
       title: "Succesfull",
       message: "Register Completed",
@@ -49,11 +35,21 @@ formBtn.addEventListener("click", (event) => {
       backgroundColor: "#20df7f",
       messageColor: "#ffff",
     });
-  }
-
-  if (usernameValid && passwordValid && emailValid) {
-    console.log();
-  } else {
+    // ! Info Of User
+    let newUserData = {
+      firstName:formFirstnameInput.value,
+      lastName:formLastnameInput.value,
+      userName: formUsernameInput.value,
+      profile: "https://imgurl.ir/uploads/e561633_banana.png",
+    };
+    fetch("http://localhost:3000/api/users", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newUserData),
+    }).then((res) => console.log(res));
+    clearInputs()
   }
 });
 
@@ -61,4 +57,10 @@ formBtn.addEventListener("click", (event) => {
 function validateEmail(email) {
   const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return regex.test(email);
+}
+
+function clearInputs() {
+  formUsernameInput.value = "";
+  formPasswordInput.value = "";
+  formEmailInput.value = "";
 }
