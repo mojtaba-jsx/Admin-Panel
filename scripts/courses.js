25aecd69b70cd9e52e7f38f519cfb3b7747490b3
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-06-08 09:12:48 +0330

    Add logic For Add New Course

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,1 +1,28 @@
-let addCourseBtn = document.querySelector('.courses-btn');
+let addCourseBtn = document.querySelector(".courses-btn");
+let courseTitle = document.querySelector(".course__add-modal-title");
+let courseTime = document.querySelector(".course__add-modal-time");
+let courseStudents = document.querySelector(".course__add-modal-students");
+let courseCategory = document.querySelector(".course__add-modal-category");
+let courseBody = document.querySelector(".course__add-modal-body");
+let coursePrice = document.querySelector(".course__add-modal-price");
+
+addCourseBtn.addEventListener("click", addNewCourse);
+
+function addNewCourse() {
+  let newCourseInfos = {
+    title: courseTitle.value,
+    body: courseBody.value,
+    time: courseTime.value,
+    price: coursePrice.value,
+    students: courseStudents.value,
+    category: courseCategory.value,
+    cover: "https://imgurl.ir/uploads/e488207_jsposter.jpg",
+  };
+  fetch("http://localhost:3000/api/courses", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(newCourseInfos),
+  }).then((res) => console.log(res));
+}
