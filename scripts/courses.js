80b5c9ee261962a40bad5ebf818d9ddcc743cfb3
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-06-09 11:01:40 +0330

    Refactor Course Item And Add Logic For Get Courses Data From Server

--- scripts/courses.js
+++ scripts/courses.js
@@ -24,5 +24,70 @@ function addNewCourse() {
       "Content-type": "application/json",
     },
     body: JSON.stringify(newCourseInfos),
-  }).then((res) => console.log(res));
+  }).then((res) => {
+    console.log(res)
+    location.reload()
+  });
+}
+
+// ! Get Courses Data
+window.addEventListener("load", getCoursesData);
+let coursesWrapper = document.querySelector(".courses__wrapper");
+function getCoursesData() {
+  fetch("http://localhost:3000/api/courses")
+    .then((res) => res.json())
+    .then((courses) => {
+      console.log(courses);
+      courses.forEach((course) => {
+        coursesWrapper.insertAdjacentHTML(
+          "beforeend",
+          `
+          <div class="courses__item">
+              <div class="courses__item-left">
+                    <img
+                      src="${course.cover}"
+                      alt="image"
+                      class="courses__item-left-image"
+                    />
+                </div>
+  
+                 <div class="courses__item-right">
+                    <div class="courses__item-right__top">
+                      <h2 class="courses__item-right__top-title">${course.title}</h2>
+                      <p class="courses__item-right__top-title-text">${course.body}</p>
+                    </div>
+  
+                    <div class="courses__item-right__bottom">
+                      <span class="courses__item-right__bottom-price">
+                        <svg><use href="#money-icon"></use></svg>
+                        ${course.price}
+                      </span>
+  
+                      <span class="courses__item-right__bottom-time">
+                        <svg><use href="#time-icon"></use></svg>
+                        ${course.time}
+                      </span>
+  
+                      <span class="courses__item-right__bottom-students">
+                        <svg><use href="#users-icon"></use></svg>
+                        ${course.students}
+                      </span>
+  
+                      <span class="courses__item-right__bottom-category">
+                        <svg><use href="#category-icon"></use></svg>
+                        #${course.category}
+                      </span>
+  
+                      <span class="courses__item-right__bottom-release">
+                        <svg><use href="#date-icon"></use></svg>
+                        ${course.created_AT
+                        }
+                      </span>
+                    </div>
+                  </div>
+                </div>
+        `
+        );
+      });
+    });
 }
